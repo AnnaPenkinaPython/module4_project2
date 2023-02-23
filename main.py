8a2dd23f603eaa360ec439a4e82b836dc3e42390
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-02-23 23:39:31 +0700

    code changed

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ import os
 from googleapiclient.discovery import build
 
 from dotenv import load_dotenv
-
-# load_dotenv()
-YOUTUBE_KEY = "AIzaSyDDVPXfBre2oshFEgqCh2NjQz9AQXmb10U"
-# API_KEY = os.getenv('YOUTUBE_KEY')
-api_key = YOUTUBE_KEY
-
+# YOUTUBE_KEY = "AIzaSyDDVPXfBre2oshFEgqCh2NjQz9AQXmb10U"
 
 class Youtube:
     channel_id = "UCByhZ-JEe5OOZSuq0uaXOng"
+
     # youtube = build('youtube', 'v3', developerKey)
     # channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
@@ -27,6 +23,7 @@ class Youtube:
         self.channel_info = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def get_service(self):
+        youtube = build('youtube', 'v3', developerKey=api_key)
         service = youtube  # build('youtube', 'v3', developerKey=api_key)
         return service
 
@@ -38,6 +35,6 @@ class Youtube:
     def print_info(self):
         return self.channel_info
 
-    if __name__ == "__main__":
-        y = Youtube(1, 2, 3, 4, 5, 6)
-        print(y.print_info())
+if __name__ == "__main__":
+    y = Youtube(1, 2, 3, 4, 5, 6)
+    print(y.print_info())
